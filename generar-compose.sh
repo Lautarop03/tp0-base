6da8f95b2e05f345ef378964d35b4d474f645ef1
Lautaro <lautaropedrozo2@gmail.com>
2025-09-02 18:08:08 -0300

    env var para server

--- generar-compose.sh
+++ generar-compose.sh
@@ -17,6 +17,7 @@ services:
     entrypoint: python3 /main.py
     environment:
       - PYTHONUNBUFFERED=1
+      - CLIENTS_NUMBER=$2
     volumes:
       - ./server/config.ini:/config.ini
     networks:
