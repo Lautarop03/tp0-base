ae51f8192101cdd56f2efcaf5a561dd8763b0cab
Lautaro <lautaropedrozo2@gmail.com>
2025-09-03 22:00:22 -0300

    clientes por threads

--- server/common/server.py
+++ server/common/server.py
@@ -1,5 +1,6 @@
 import socket
 import logging
+import threading
 from .utils import store_bets, load_bets, has_won
 from .protocol import Protocol
 from .protocol import (OPCODE_INIT_CLIENT, OPCODE_BATCH_BETS, OPCODE_FINISHED_BETS, OPCODE_REQUEST_WINNERS)
@@ -10,10 +11,10 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
-        self._client_socket = None
+        self._clients_threads = []
+        self._clients_socket = []
         self._clients_number = clients_number
         self._agency_ready = [False] * clients_number
-        self.protocol = Protocol(self._client_socket)
 
     def run(self):
         """
@@ -26,9 +27,11 @@ class Server:
 
         while True:
             client_sock = self.__accept_new_connection()
-            self._client_socket = client_sock
-            self.__handle_client_connection(client_sock)
-
+            client_th = threading.Thread(target=self.__handle_client_connection, args=(client_sock,))
+            client_th.start()
+            self._clients_threads.append(client_th)
+            self._clients_socket.append(client_sock)
+        
 
     def __handle_client_connection(self, client_sock):
         """
@@ -37,26 +40,24 @@ class Server:
         If a problem arises in the communication with the client, the
         client socket will also be closed.
         """
+        protocol = Protocol(client_sock)
         try:
-            self.protocol.socket = client_sock
-
             while True:
-                opcode = self.protocol.read_opcode()
+                opcode = protocol.read_opcode()
 
                 if opcode == OPCODE_INIT_CLIENT:
-                    client_id = self.__handle_init_client() 
+                    client_id = self.__handle_init_client(protocol) 
 
                 elif opcode == OPCODE_BATCH_BETS:
-                    if not self.__handle_batch_msg():
+                    if not self.__handle_batch_msg(protocol):
                         return
                 
                 elif opcode == OPCODE_FINISHED_BETS: 
                     self.__handle_finished_bets(client_id)
-                    return
 
                 elif opcode == OPCODE_REQUEST_WINNERS:
-                    self.__handle_request_winners(client_id)
-                    return
+                    if self.__handle_request_winners(protocol, client_id):
+                        return
                     
         except OSError as e:
             logging.error(f"action: receive_message | result: fail | error: {e}")
@@ -82,28 +83,31 @@ class Server:
     def stop(self):
         self._server_socket.close()
         logging.info("action: stop | result: success | detail: server socket was closed")
-        self._client_socket.close()
+        for client in self._clients_socket:
+            client.close()
+        for thread in self._clients_threads:
+            thread.join()
         logging.info(f"action: stop | result: success | detail: client socket was closed")
 
 
     # ------ Handlers for each opcode ------ #
 
-    def __handle_batch_msg(self) -> bool:
+    def __handle_batch_msg(self, protocol: Protocol) -> bool:
         """Handle receiving and storing a batch of bets. Returns False if should stop loop."""
-        bets = self.protocol.read_batch_msg()
+        bets = protocol.read_batch_msg()
 
         if bets is None or len(bets) == 0:
             logging.warning("action: apuesta_recibida | result: fail | cantidad: 0")
-            self.protocol.send_batch_confirmation(False)
+            protocol.send_batch_confirmation(False)
             return False
 
         store_bets(bets)
         logging.info(f"action: apuesta_recibida | result: success | cantidad: {len(bets)}")
-        self.protocol.send_batch_confirmation(True)
+        protocol.send_batch_confirmation(True)
         return True
 
 
-    def __handle_request_winners(self, client_id: int) -> None:
+    def __handle_request_winners(self, protocol: Protocol, client_id: int) -> bool:
         if all(self._agency_ready):
             # Todas las agencias estan listas
             bets = load_bets()
@@ -111,14 +115,16 @@ class Server:
             for bet in bets:
                 if bet.agency == client_id and has_won(bet):
                     ganadores.append(bet.document)
-            self.protocol.send_winners(ganadores)
+            protocol.send_winners(ganadores)
+            return True
         else:
             # Las agencias todavia no estan listas
-            self.protocol.send_wait()
+            protocol.send_wait()
+            return False
 
 
-    def __handle_init_client(self) -> int:
-        return self.protocol.receive_client_id()  
+    def __handle_init_client(self, protocol: Protocol) -> int:
+        return protocol.receive_client_id()  
     
 
     def __handle_finished_bets(self, client_id: int) -> None:
